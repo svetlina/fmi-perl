d08194103486bad7b08b25550a577670f885742c
Svetlina <svetlina_dukova@abv.bg>
2015-07-07 11:01:29 +0300

    adding client pl

---
+++ client.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use v5.012;
+use IO::Socket::INET;
+
+# auto-flush on socket
+$| = 1;
+
+my $server_port="5076";
+my $server_host="127.0.0.1";
+
+my $client_socket = IO::Socket::INET->new(
+	PeerHost => $server_host,
+	PeerPort => $server_port,
+	Proto => 'tcp',
+);
+
+unless($client_socket){
+	die "Couldn't create client socket - $!";
+}
+
+say "CLIENT connected to server";
+
+my ($server_reply,$client_msg);
+
+while(<STDIN>){
+	$client_msg=$_;
+	$client_socket->send($client_msg);
+
+	$client_socket->recv($server_reply,1024);
+	say "$server_reply";
+}
+
+sleep (10);
+$client_socket->close();
